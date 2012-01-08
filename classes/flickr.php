2ff3850c300a562039d79fede5129bebec864608
joelmorland <joelmorland@gmail.com>
2012-01-09 10:37:22 +1100

    Updated pagination due to flickr API 4000 search result restriction

--- classes/flickr.php
+++ classes/flickr.php
@@ -14,32 +14,44 @@ class flickr extends phpFlickr
         
         //check that photos returned
         if (is_array($photos) && count($photos)) {
-            $disp = ($page*5-5).' - '.($page*5); #displaying photos
-            echo "<div id='notice'>Found {$photos['total']} Photos | Displaying $disp</div>";
-            echo '<div id="gallery">';
+            #currently displayed photos
+            $disp = ($page*5-5).' - '.($page*5);
+            echo "<div id='notice'>
+                        Found {$photos['total']} photos for '$search' [limited to 4000]
+                        <br/>Displaying $disp
+                  </div>";
+            
+            ## GALLERY ##
+            echo "<div id='gallery'>";
             
             #echo photos
             foreach ($photos['photo'] as $photo) 
             {
                 echo "<a href='index.php?image={$photo['id']}' target='_blank'><img class='galleryImage' src='".$this->buildPhotoURL($photo,'thumbnail')."' /></a>";
             }
-            echo '</div>';
             
-            $this->pagination($page, $photos['total'], $search);
+            echo '</div>';
+            ## END GALLERY ##
+            
+            $this->pagination($page, $photos['pages'], $search);
         }
     }
     
-    function pagination($cur, $total, $search) {
+    function pagination($cur, $pages, $search) {
+        #flickr API only allows 4000 results to be returned - limit to 4000 otherwise dummy photos are displayed by flickr
+        $pages=($pages>4000/5)?4000/5:$pages;         
+        
         echo '<div class="pagination">';
         
             echo ($cur>4)?"<a href='index.php?s=$search&p=1'>1</a>":'';
             
             for ($i=$cur-2; $i<=$cur+2; $i++) {
-                if ($i<1 || $i>$total) continue;
+                if ($i<1 || $i>$pages) continue; #ensure page not less than one or greater than last page
                 echo ($i!=$cur)?"<a href='index.php?s=$search&p=$i'>$i</a>":"<b>$i</b>";
             } 
-            
-            echo ($cur!==$total)?"...<a href='index.php?s=$search&p=$total'>$total</a>":'';
+
+            #ensure current
+            echo ($cur<=($pages-2))?"...<a href='index.php?s=$search&p=$pages'>$pages</a>":'';
             
         echo '</div>';
     }
